483ee3bf56ceeeadd44833091814a1b657576dae
mbarber84 <mbarber_84@hotmail.co.uk>
2023-02-13 07:56:22 +0000

    Comments added using openAI

--- src/main/java/com/mycompany/platformgame/PlatformGame.java
+++ src/main/java/com/mycompany/platformgame/PlatformGame.java
@@ -1,5 +1,7 @@
 package com.mycompany.platformgame;
 
+// The class contains a single method called main which is the entry point of a Java application.
+
 public class PlatformGame {
 
     public static void main(String[] args) {
