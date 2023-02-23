be8203b25ce2e677105e4e27dcaa36e3c721baa6
mbarber84 <mbarber_84@hotmail.co.uk>
2023-02-23 11:25:18 +0000

    Comments added to package com.mycompany.platformgame using openAI

--- src/main/java/Inputs/KeyboardInputs.java
+++ src/main/java/Inputs/KeyboardInputs.java
@@ -8,7 +8,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 /**
- *
+ * This Java code defines a class named KeyboardInputs that implements the KeyListener interface. Its constructor takes a GamePanel object as a parameter, and it overrides three     * methods of the KeyListener interface: keyTyped(), keyPressed(), and keyReleased(). The keyPressed() and keyReleased() methods use a switch statement to call methods in the        * Gamestate class based on the current state of the game (either MENU or PLAYING).
  * @author mbarb
  */
 public class KeyboardInputs implements KeyListener {
@@ -37,6 +37,7 @@ public class KeyboardInputs implements KeyListener {
                 break;
         }
     }
+    //Inside the method, there is a switch statement that checks the value of a Gamestate variable. Depending on the value, the method calls the keyPressed() method of either the         Menu or Playing object contained in a GamePanel object.
 
     @Override
     public void keyReleased(KeyEvent e) {
@@ -51,5 +52,5 @@ public class KeyboardInputs implements KeyListener {
                 break;
         }
     }
-
+    // The method uses a switch statement to determine the current state of the game and call the keyReleased() method of the appropriate object based on the current state.
 }
--- src/main/java/Inputs/MouseInputs.java
+++ src/main/java/Inputs/MouseInputs.java
@@ -9,7 +9,7 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 /**
- *
+ * This class that implements both the MouseListener and MouseMotionListener interfaces. It defines methods for handling mouse events such as clicks, presses, releases, and          * movements. It uses a GamePanel object and checks the current state of the game to determine which event handlers to call. If the game is in the PLAYING state, it calls the mouse  * event handlers for the playing state. If it is in the MENU state, it calls the mouse event handlers for the menu state.
  * @author mbarb
  */
 public class MouseInputs implements MouseListener, MouseMotionListener {
@@ -19,6 +19,7 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
     public MouseInputs(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
     }
+    //Constructor that initializes a MouseInputs object with a GamePanel object, which is used to access the game's current state.
 
     @Override
     public void mouseClicked(MouseEvent e) {
@@ -30,6 +31,7 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
                 break;
         }
     }
+    //Gets called when the mouse is clicked. It checks the current state of the game and calls the appropriate mouse click handler method for that state.
 
     @Override
     public void mousePressed(MouseEvent e) {
@@ -44,6 +46,7 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
                 break;
         }
     }
+    //Gets called when a mouse button is pressed down. It checks the current state of the game and calls the appropriate mouse press handler method for that state.
 
     @Override
     public void mouseReleased(MouseEvent e) {
@@ -58,6 +61,7 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
                 break;
         }
     }
+    //Gets called when a mouse button is released. It checks the current state of the game and calls the appropriate mouse release handler method for that state.
 
     @Override
     public void mouseEntered(MouseEvent e) {
@@ -79,6 +83,7 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
                 break;
         }
     }
+    //Gets called when the mouse is dragged (i.e. moved while holding down a button). It checks the current state of the game and calls the appropriate mouse drag handler method         for that state.
 
     @Override
     public void mouseMoved(MouseEvent e) {
@@ -93,5 +98,5 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
                 break;
         }
     }
-
+    //Gets called when the mouse is moved (without holding down any button). It checks the current state of the game and calls the appropriate mouse move handler method for that         state.
 }
